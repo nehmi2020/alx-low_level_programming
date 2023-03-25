d6a43d1df737811dc219cc3a3a13fd629b0345c3
nehmi2020 <nehmi2020@yahoo.com>
2023-03-25 15:32:06 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * main - Test function for positive or negative
 * Return: 0
@@ -7,7 +8,9 @@
 int main(void)
 {
 	int i;
+
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
